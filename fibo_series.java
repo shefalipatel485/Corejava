c449455f3a5c522bf010e31833058cf195963ef2
shefalipatel485 <shefalip2206@gmail.com>
2025-01-29 11:06:11 +0530

    Add files via upload

---
+++ fibo_series.java
@@ -0,0 +1,25 @@
+package Basics;
+
+public class fibo_series {
+
+	public static void main(String[] args) {
+
+		
+		int a=0,b=1;
+		
+		for(int i=0;i<=8;i++) {
+			
+			int c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+			
+		}
+		//factorial
+		
+	
+		
+		
+		
+	}
+}
