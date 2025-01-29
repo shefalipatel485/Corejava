0714b6e5bc17b74d694856e2ed854da679a7a373
shefalipatel485 <shefalip2206@gmail.com>
2025-01-29 11:59:18 +0530

    Add files via upload

---
+++ patterns.java
@@ -0,0 +1,24 @@
+package Basics;
+
+public class patterns {
+public static void main(String[] args) {
+	
+	
+
+	for(int i=0;i<=5;i++) {
+		
+		for(int j=5;j>=i;j--) {
+			
+			
+			System.out.print("*");
+			
+		}
+		//new line
+		System.out.println();
+		
+	}
+	
+
+	
+}
+}
